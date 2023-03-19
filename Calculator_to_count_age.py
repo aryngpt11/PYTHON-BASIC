2493368132a0b6b7543e05f62fb3d8d2d0f3ff27
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-03-19 14:00:34 +0530

    age_cal

---
+++ Calculator_to_count_age.py
@@ -0,0 +1,6 @@
+age=int(input("Enter the age: "))
+years_remaining=90-age
+days=years_remaining*365
+weeks=years_remaining*52
+months=years_remaining*12
+print(f"you have remaning {days} days,{weeks} weeks and {months} months")
