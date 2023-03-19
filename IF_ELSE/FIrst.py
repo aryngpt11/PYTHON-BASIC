344dc9ea9e6241de1c788b629b7bd194a14eccbe
aryngpt11 <103244478+aryngpt11@users.noreply.github.com>
2023-03-19 20:19:44 +0530

    Even_odd

---
+++ IF_ELSE/FIrst.py
@@ -0,0 +1,6 @@
+print("Welcome to the rolercoaster")
+height=int(input("enter the height in cm: "))
+if height>=120:
+    print("U are eligible to ride")
+else:
+    print("u are not eligible for the ride")    
---
+++ IF_ELSE/ODD_EVEN.py
@@ -0,0 +1,5 @@
+no=int(input("Enter the no: "))
+if no%2==0:
+    print(f"{no} is an even number")
+else:
+    print(f"{no} is an odd no")
